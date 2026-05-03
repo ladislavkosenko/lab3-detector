997f745e6bfe1adbce70c5f1d2bacf431fb4c2ba
Vladyslav <ladislavkosenko@gmail.com>
2026-05-04 01:14:18 +0300

    fix: optimize logger initialization and clean code

--- internal/processor/metadata.go
+++ internal/processor/metadata.go
@@ -6,12 +6,13 @@ import (
 	"time"
 
 	"lab3-detector/internal/stats"
+
+	"go.uber.org/zap"
 )
 
 var LeakCache = make(map[string][]byte) // КОНФЛІКТНА ЗМІНА З MAIN
-
-// 1. ОПТИМІЗАЦІЯ: Компілюємо регулярку один раз глобально
 var imageRegexp = regexp.MustCompile(`^image_data_\d+_timestamp_\d+$`)
+var logger, _ = zap.NewProduction()
 
 func RunWorkerPool(count int) {
 	for i := 0; i < count; i++ {
@@ -28,16 +29,20 @@ func RunWorkerPool(count int) {
 func processImage(workerID int) {
 	data := fmt.Sprintf("image_data_%d_timestamp_%d", workerID, time.Now().UnixNano())
 
-	// 2. Використовуємо вже скомпільовану регулярку
 	if imageRegexp.MatchString(data) {
 		key := fmt.Sprintf("key_%d", time.Now().UnixNano())
-		LeakCache[key] = make([]byte, 10) // Зменшив виділення пам'яті до 10 байт, щоб не падав ПК під час тестів
+		LeakCache[key] = make([]byte, 10)
 
 		stats.IncrementProcessed("image")
+
+		logger.Info("Image successfully processed",
+			zap.Int("workerID", workerID),
+			zap.String("key", key),
+		)
 	}
 }
 
-// 3. ДОДАЄМО: Функція для Benchmark-у старої (повільної) версії
+// Функція для Benchmark-у (якщо вона була)
 func processImageSlow(workerID int) {
 	data := fmt.Sprintf("image_data_%d_timestamp_%d", workerID, time.Now().UnixNano())
 	matched, _ := regexp.MatchString(`^image_data_\d+_timestamp_\d+$`, data)
